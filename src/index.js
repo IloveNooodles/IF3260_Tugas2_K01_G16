cb48a1a4d1bcfd5c79621b55b4ecdd1f125a1486
dzaky.fattan <70198829+DzakyFattan@users.noreply.github.com>
2023-03-15 18:25:48 +0700

    feat: state, event listeners

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ void main(void) {
 }
 `;
 
-const fragment_shader_3d_no_ligthing = `
+const fragment_shader_3d_no_lighting = `
 precision mediump float;
 uniform vec3 userColor;
 varying float colorFactor;
@@ -40,14 +40,54 @@ void main(void) {
 `;
 
 /* ======= Global object ======= */
+var state;
+
+function setDefaultState() {
+  /* Setup default state for webgl canvas */
+  state = {
+    model: {},
+    transform: {
+      translate: [0, 0, 0], // x, y, z
+      rotate: [0, 0, (Math.PI / 180) * 45], // x, y, z
+      scale: [1, 1, 1], // x, y, z
+    },
+    projection: "orthographic", // orthographic, oblique, or perspective
+    lighting: true,
+    pickedColor: [0.0, 0.0, 0.0, 1.0], // r, g, b, a
+  };
+}
 
 /* ======= Get Document Object Model ======= */
 const canvas = document.getElementById("canvas");
 const reset = document.getElementById("reset");
+const projectionRadio = document.getElementsByName("projection");
+const lightingCheckbox = document.getElementById("lighting");
+const colorPicker = document.getElementById("color-picker");
 
 /* ======= Event Listener ======= */
 reset.addEventListener("click", () => {
   clear();
+});
+
+projectionRadio.forEach((radio) => {
+  radio.addEventListener("change", () => {
+    state.projection = radio.value;
+  });
+});
+
+colorPicker.addEventListener("change", () => {
+  const color = colorPicker.value;
+  /* convert hex to rgb, normalize */
+  state.pickedColor = [
+    parseInt(color.substring(1, 3), 16) / 255,
+    parseInt(color.substring(3, 5), 16) / 255,
+    parseInt(color.substring(5, 7), 16) / 255,
+    1.0,
+  ];
+});
+
+lightingCheckbox.addEventListener("change", () => {
+  state.lighting = lightingCheckbox.checked;
 });
 
 /* ======= WebGL Functions ======= */
@@ -58,12 +98,12 @@ window.onload = function () {
   if (!gl) {
     alert("WebGL not supported");
   }
-
+  setDefaultState();
   clear();
 };
 
 function clear() {
-  /* Setup black screan for webgl canvas */
+  /* Setup black screen for webgl canvas */
   gl.clearColor(0, 0, 0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
@@ -75,10 +115,7 @@ function loadShader(gl, type, input) {
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error(
-      "ERROR compiling vertex shader!",
-      gl.getShaderInfoLog(vertexShader)
-    );
+    console.error("ERROR compiling vertex shader!", gl.getShaderInfoLog(vertexShader));
     return null;
   }
 
