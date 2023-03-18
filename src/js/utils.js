598b4041e972c6c85a1f141c0778bf22fc394e0d
IloveNooodles <mgarebaldhie80@gmail.com>
2023-03-18 21:22:51 +0700

    feat: adding function to locate centroid

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,3 +5,32 @@ function radToDeg(radians) {
 function degToRad(degrees) {
   return (degrees * Math.PI) / 180;
 }
+
+function resizeCanvas(canvas) {
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    canvas.width = width;
+    canvas.height = height;
+  }
+}
+
+/* matrix is 3d */
+function locateCentroid(matrix) {
+  let x = 0;
+  let y = 0;
+  let z = 0;
+  let vertexCount = matrix.length;
+  for (let i = 0; i < vertexCount; i++) {
+    x += matrix[i][0];
+    y += matrix[i][1];
+    z += matrix[i][2];
+  }
+
+  x = x / vertexCount;
+  y = y / vertexCount;
+  z = z / vertexCount;
+
+  return { x, y, z };
+}
